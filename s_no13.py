66e3a3222ed82b94e27632c05b96bf6856ebb276
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2022-07-12 21:53:08 +0530

    Create s_no13.py

---
+++ s_no13.py
@@ -0,0 +1,10 @@
+f=open('abc.txt','r')
+counting=0
+flag=True
+while flag==True:
+    temp=f.readline().lower()
+    temp1=temp.split(" ")
+    for i in temp1:
+        if i.strip()=='my':
+            counting+=1
+        else: continue
