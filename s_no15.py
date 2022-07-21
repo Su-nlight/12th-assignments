d4019c064bd0bfd7db7ba4032375ba1ec7bcc579
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2022-07-21 10:55:27 +0530

    Create s_no15.py

---
+++ s_no15.py
@@ -0,0 +1,8 @@
+f=open("idksomething.txt",'r')
+temp=f.readlines()
+counting= [0,0]
+
+for i in temp:
+	counting[0]+=i.strip().lower().count("to")
+	counting[1]+=i.strip().lower().count("the")
+print("Number of occurences of 'to' is{}\noccurences of 'the' is {}".format(counting[0],counting[1]))
