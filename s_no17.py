b819a3bc1f6e7f95f15ff408978167f6e37480f4
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2022-07-21 11:04:15 +0530

    Create s_no17.py

---
+++ s_no17.py
@@ -0,0 +1,10 @@
+def longest_line(filename) :
+    lo_line = ""
+    f=open(filename,'r')
+    
+    for line in f.readlines():
+        if len(line) > len(lo_line):
+            lo_line = line
+    return(longest)
+  
+print(longest_line("abc_file.txt"))
