fe8f6b9098a1a6c0d75b9c5f7bf4ba8aa6987725
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2022-11-09 18:04:20 +0530

    Update s_no20.py

--- s_no20.py
+++ s_no20.py
@@ -1,37 +1,45 @@
 import pickle
 import sys
 
+
 def make_file():
-    x=input("Enter name of file without Extension : ").strip()
-    l=[]
-    f1 = open(x + ".dat", 'wb')
-    flag=True
-    while flag==True:
-        l.append(input("Enter content of file : "))
-    pickle.dump(f1,l)
+  x = input("Enter name of file without Extension : ").strip()
+  l = []
+  f1 = open(x + ".dat", 'wb')
+  flag = True
+  while flag == True:
+    l.append(input("Enter content of file : "))
+    ch = input("would you like to continue? (y/n) : ")
+    if ch == "n":
+      flag = False
+  pickle.dump(f1, l)
+
 
 def read_file():
-    x=input("Enter filename without extension").strip()
-    f1=open(x+".dat",'rb')
-    content=pickle.load(f1)
-    print(content)
+  x = input("Enter filename without extension").strip()
+  f1 = open(x + ".dat", 'rb')
+  content = pickle.load(f1)
+  print(content)
+
 
 def main():
-    print("1 --> Create a Binary file\n2 --> Read a Binary file\n3 --> Exit the program")
-    try:
-        ch=int(input("Enter Choice: "))
-        if ch==1:
-            make_file()
-        elif ch==2:
-            read_file()
-        elif ch==3:
-            sys.exit(0)
-        else:
-            print("Invalid choice")
-    except ValueError:
-        print("Choice is not an integer")
-    except:
-        sys.exit(1)
-    
-if "__name__"=="__main__":
-    main()
+  print(
+    "1 --> Create a Binary file\n2 --> Read a Binary file\n3 --> Exit the program"
+  )
+  try:
+    ch = int(input("Enter Choice: "))
+    if ch == 1:
+      make_file()
+    elif ch == 2:
+      read_file()
+    elif ch == 3:
+      sys.exit(0)
+    else:
+      print("Invalid choice")
+  except ValueError:
+    print("Choice is not an integer")
+  except:
+    sys.exit(1)
+
+
+main()
